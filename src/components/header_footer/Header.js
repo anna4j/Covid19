e321db729921f19dd3fb232c9e1a0a29a2257609
anna4j <anushkajadhav004@gmail.com>
2020-06-12 19:16:03 +0530

    header done

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,14 +2,50 @@ import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import SideDrawer from './SideDrawer';
 
 class Header extends Component {
+
+    state = {
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    /* vanilla js*/
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    /*for making header transparent when scrolling*/
+    handleScroll = () => {
+        if (window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            })
+        } else {
+            this.setState({
+                headerShow: false
+            })
+        }
+
+    }
+
+
+
+
+    toggleDrawer = (value) => {
+        this.setState({
+            drawerOpen: value
+        })
+    }
+
     render() {
         return (
             <AppBar
                 position="fixed"
                 style={{
-                    backgroundColor: '#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                     boxShadow: 'none',
                     padding: "10px, 0px"
                 }}
@@ -20,6 +56,18 @@ class Header extends Component {
                         <div className="header_logo_icon">MUSICAL EVENTS</div>
 
                     </div>
+                    <IconButton
+                        aria-label="Menu"
+                        color="inherit"
+                        onClick={() => this.toggleDrawer(true)}
+
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value) => this.toggleDrawer(value)}
+                    />
                 </Toolbar>
             </AppBar>
 
