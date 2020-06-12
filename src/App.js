27b54b922dc19d23a4e35a9471e85b3ede7baccd
anna4j <anushkajadhav004@gmail.com>
2020-06-13 00:00:25 +0530

    slider almost done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './resources/styles.css';
+import { Element } from 'react-scroll';
+
 
 import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: "cornflowerblue" }}>
+      <div className="App" style={{ height: "1500px", background: 'cornflowerblue' }}>
         <Header />
+        <div>
+          <Featured />
+        </div>
+
+
       </div>
+
+
     );
   }
 }
-
+export default App;
 
