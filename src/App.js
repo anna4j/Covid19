45752423526e207c1b04e9afe941ca8c921af7e9
anna4j <anushkajadhav004@gmail.com>
2020-06-13 17:37:31 +0530

    highlights added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import { Element } from 'react-scroll';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueNfo from './components/venue NFO'
+import Highlights from './components/Highlights'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "1500px", background: 'cornflowerblue' }}>
+      <div className="App" >
 
         <Header />
         <Element name="featured">
           <Featured />
         </Element>
         <VenueNfo />
+        <Highlights />
+
 
 
 
