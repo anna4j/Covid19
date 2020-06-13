5549276bbd0ad34468220de6485df8cca669226b
anna4j <anushkajadhav004@gmail.com>
2020-06-13 21:54:36 +0530

    scroll to added

--- src/App.js
+++ src/App.js
@@ -18,22 +18,29 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-
         <Header />
+
         <Element name="featured">
           <Featured />
         </Element>
-        <VenueNfo />
-        <Highlights />
-        <Pricing />
-        <Location />
+
+        <Element name="venuenfo">
+          <VenueNfo />
+        </Element>
+
+        <Element name="highlights">
+          <Highlights />
+        </Element>
+
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+
+        <Element name="location">
+          <Location />
+        </Element>
+
         <Footer />
-
-
-
-
-
-
 
       </div>
 
