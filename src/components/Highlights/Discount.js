b12b9b62a16e21062f6c9b2703aff9b3d92f87f8
anna4j <anushkajadhav004@gmail.com>
2020-06-13 18:36:53 +0530

    discount sec added

---
+++ src/components/Highlights/Discount.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Fade from 'react-reveal/Fade'
+import Slide from 'react-reveal/Slide'
+
+
+
+export default class Discount extends Component {
+
+    state = {
+        discountStart: 0,
+        discountEnd: 30
+    }
+    porcentage = () => {
+
+        if (this.state.discountStart < this.state.discountEnd) {
+            this.setState({
+                discountStart: this.state.discountStart + 1
+            })
+
+        }
+    }
+    componentDidUpdate() {
+        setTimeout(() => {
+            this.porcentage()
+        }, 40)
+    }
+
+    render() {
+        return (
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                    <Fade
+                        onReveal={() => this.porcentage()}
+                    >
+
+                        <div className="discount_porcentage">
+                            <span>{this.state.discountStart}%</span>
+                            <span>OFF</span>
+                        </div>
+                    </Fade>
+
+                    <Slide right>
+
+                        <div className="discount_description">
+                            <h3>Purchase tickets before 30th April</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+                            <div>
+                                button
+                            </div>
+                        </div>
+                    </Slide>
+                </div>
+
+            </div>
+        )
+    }
+}
